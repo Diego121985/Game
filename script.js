7e291edf03effab7ea5e607bc570653dce97c7ef
Diego121985 <diegoleite59986@gmail.com>
2024-08-14 19:23:11 -0300

    Game

---
+++ script.js
@@ -0,0 +1,11 @@
+function abrir(){
+    document.querySelector("form").style.opacity = 1
+    document.querySelector("div").style.opacity = 0.4
+
+}
+function fechar(){
+    document.querySelector("form").style.opacity = 0
+    document.querySelector("div").style.opacity = 1
+
+
+}
